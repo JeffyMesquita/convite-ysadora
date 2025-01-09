2e49d83b0542a010d17263d77dd3a995f287dc4a
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-01-09 14:56:36 -0300

    style: change color

--- src/components/Embarrassment.tsx
+++ src/components/Embarrassment.tsx
@@ -8,7 +8,7 @@ export default function Embarrassment() {
       whileTap={{ scale: 0.9 }}
       className="flex flex-col items-center"
     >
-      <div className="w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 rounded-full bg-gradient-to-r from-violet-300 to-violet-500 flex items-center justify-center shadow-lg p-2 shadow-violet-500">
+      <div className="w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 rounded-full bg-gradient-to-r from-rose-300 to-rose-500 flex items-center justify-center shadow-lg p-2 shadow-rose-500">
         <Image
           src="/images/embarrassment.png"
           alt="Vergonha"
@@ -17,7 +17,7 @@ export default function Embarrassment() {
           className="w-full h-full object-cover object-top"
         />
       </div>
-      <p className="text-center font-bold text-violet-600 mt-1 text-xs sm:text-sm sm:mt-2">
+      <p className="text-center font-bold text-rose-600 mt-1 text-xs sm:text-sm sm:mt-2">
         Vergonha
       </p>
     </motion.div>
